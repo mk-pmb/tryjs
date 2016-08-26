341915ec6b6a34a5287c1c4f990bb0a9d1a38015
mk-pmb <mk-pmb@users.noreply.github.com>
2016-08-26 17:08:12 +0200

    dom/xmldefuse.js

---
+++ dom/xmldefuse.js
@@ -0,0 +1,8 @@
+﻿/*jslint indent: 2, maxlen: 80, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+module.exports = function xmldefuse(text) {
+  return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g,
+    '&gt;').replace(/'/g, '&#39;').replace(/"/g, '&quot;');
+};
